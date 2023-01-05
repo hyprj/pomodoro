64c37e7cae92f085136b8de889d2ed3cc832e5bd
hyprj <Krzysztofzur21@gmail.com>
2023-01-05 22:46:09 +0100

    test: add NumberInput tests

--- src/common/components/form/__tests__/NumberInput.test.tsx
+++ src/common/components/form/__tests__/NumberInput.test.tsx
@@ -1,26 +1,42 @@
 import { fireEvent, render } from "@testing-library/react";
 import { NumberInput } from "../NumberInput";
 
-test("should render component with defaultValue set", () => {
-  const { getByRole } = render(<NumberInput label="test" defaultValue={10} />);
-  const input = getByRole("spinbutton");
-  expect(input).toHaveValue(10);
-});
+const props = {
+  min: 1,
+  max: 10,
+  defaultValue: 10,
+  onChange: () => undefined,
+  label: "test",
+};
 
-test("should render label with sr-only class for renderLabel set to false", () => {
-  const { getByText } = render(
-    <NumberInput label="test" renderLabel={false} />
-  );
-  const label = getByText("test");
-  expect(label).toHaveClass("sr-only");
-});
+describe("NumberInput", () => {
+  test("should render component with defaultValue set", () => {
+    const { getByRole } = render(<NumberInput {...props} defaultValue={5} />);
+    const input = getByRole("spinbutton");
+    expect(input).toHaveValue(5);
+  });
 
-test("should call onChange when input is changed", () => {
-  const onChange = jest.fn();
-  const { getByLabelText } = render(
-    <NumberInput onChange={onChange} label="test" />
-  );
-  const input = getByLabelText("test");
-  fireEvent.change(input, { target: { value: 10 } });
-  expect(onChange).toBeCalled();
+  test("should render label with sr-only class for renderLabel set to false", () => {
+    const { getByText } = render(
+      <NumberInput {...props} renderLabel={false} />
+    );
+    const label = getByText("test");
+    expect(label).toHaveClass("sr-only");
+  });
+
+  test("should call onChange when input is changed", () => {
+    const onChange = jest.fn();
+    const { getByLabelText } = render(
+      <NumberInput
+        label="test"
+        min={1}
+        max={10}
+        defaultValue={10}
+        onChange={onChange}
+      />
+    );
+    const input = getByLabelText("test");
+    fireEvent.change(input, { target: { value: 10 } });
+    expect(onChange).toBeCalled();
+  });
 });
